3db64e52b375a4997ade6eeff3e731f1410690f5
Julien Bouquillon <contact@revolunet.com>
2011-04-07 02:56:56 -0700

    Edited setup.py via GitHub

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = '0.4dev'
 setup(name='extdirect.django',
       version=version,
       description="Ext.Direct implementation for Django",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.md").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
@@ -17,9 +16,9 @@ setup(name='extdirect.django',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Santiago Videla',
-      author_email='santiago.videla@gmail.com',
-      url='http://github.com/gsancho/extdirect.django/tree/master',
+      author='Julien Bouquillon',
+      author_email='julien@revolunet.com',
+      url='http://github.com/revolunet/extdirect.django/tree/master',
       license='BSD',      
       namespace_packages=['extdirect'],
       packages=find_packages(exclude=['ez_setup']),
