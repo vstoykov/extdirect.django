1de2217e5d39d5c6b37e163b9833b06660880f59
Venelin Stoykov <venelin@magicbg.com>
2012-02-28 12:07:47 +0200

    merge __init__.py from extdirect v4 and current extdirect.django

--- extdirect/django/__init__.py
+++ extdirect/django/__init__.py
@@ -1,7 +1,33 @@
-#
+from django.utils.importlib import import_module
+
 from providers import ExtRemotingProvider, ExtPollingProvider
 from store import ExtDirectStore
 from crud import ExtDirectCRUD
 from decorators import remoting, polling, crud
+from router import DjangoDirectRouter as DirectRouter
+from router import register_router
 
+LOADING = False
+
+def autodiscover(name='direct'):
+    """
+    Autodiscover direct modules of django app
+    """
+    global LOADING
+    if LOADING:
+        return
+    LOADING = True
+    import imp
+    from django.conf import settings
+    for app in settings.INSTALLED_APPS:
+        try:
+            app_path = import_module(app).__path__
+        except AttributeError:
+            continue
+        try:
+            imp.find_module(name, app_path)
+        except ImportError:
+            continue
+        import_module("%s.%s" % (app, name))
+    LOADING = False
 
